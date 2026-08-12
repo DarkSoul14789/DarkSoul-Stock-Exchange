b73a69e61fab19848336af674cfaacc06e9e6e99
DarkSoul14789 <shubhrajivshah@gmail.com>
2026-08-12 19:40:52 +0530

    added test cases

--- tests/test_orderbook.cpp
+++ tests/test_orderbook.cpp
@@ -19,3 +19,68 @@ TEST_CASE("a single resting limit order becomes the best price", "[orderbook][ba
     REQUIRE(book.size() == 1);
 }
 
+TEST_CASE("a marketable limit order fully matches against the resting order", "[orderbook][matching]") {
+    OrderBook book;
+    Order sell(1, Order::Side::Sell, Order::Type::Limit, 100.0, 10);
+    auto trades = book.addOrder(sell);
+    
+    Order buy(2, Order::Side::Buy, Order::Type::Limit, 100.0, 10);
+    auto trades1 = book.addOrder(buy);
+
+    REQUIRE(trades1.size() == 1);
+    REQUIRE(trades1[0].price == 100.0);
+    REQUIRE(trades1[0].quantity == 10);
+    REQUIRE(book.size() == 0);
+}
+
+TEST_CASE("a limit order partially fills and the remainder rests", "[orderbook][matching]") {
+    OrderBook book;
+    Order sell(1, Order::Side::Sell, Order::Type::Limit, 100.0, 10);
+    auto trades = book.addOrder(sell);
+    
+    Order buy(2, Order::Side::Buy, Order::Type::Limit, 100.0, 4);
+    auto trades1 = book.addOrder(buy);
+
+    REQUIRE(trades1.size() == 1);
+    REQUIRE(trades1[0].quantity == 4);
+    REQUIRE(book.size() == 1);
+    REQUIRE(book.quantityAtBestAsk() == 6);
+}
+
+TEST_CASE("price-time priority: earlier order at the same price fills first", "[orderbook][matching]") {
+    OrderBook book;
+    Order sell(1, Order::Side::Sell, Order::Type::Limit, 100.0, 5);
+    auto trades = book.addOrder(sell);
+    
+    Order sell1(2, Order::Side::Sell, Order::Type::Limit, 100.0, 5);
+    auto trades1 = book.addOrder(sell1);
+    REQUIRE(book.quantityAtBestAsk() == 10);
+
+    Order buy(3, Order::Side::Buy, Order::Type::Limit, 100.0, 5);
+    auto trades2 = book.addOrder(buy);
+
+    REQUIRE(trades2.size() == 1);
+    REQUIRE(trades2[0].buyOrderId == 3);
+    REQUIRE(trades2[0].sellOrderId == 1);
+    REQUIRE(book.quantityAtBestAsk() == 5);
+}
+
+TEST_CASE("a market order matches at the best available price", "[orderbook][matching]") {
+    OrderBook book;
+    Order sell(1, Order::Side::Sell, Order::Type::Limit, 101.0, 5);
+    book.addOrder(sell);
+
+    Order buy(2, Order::Side::Buy, Order::Type::Market, -1, 5);
+    auto trades = book.addOrder(buy);
+
+    REQUIRE(trades.size() == 1);
+    REQUIRE(trades[0].price == 101.0);
+}
+
+TEST_CASE("a market order with insufficient liquidity does not rest", "[orderbook][matching]") {
+    OrderBook book;
+    Order buy(1, Order::Side::Buy, Order::Type::Market, -1, 5);
+    auto trades = book.addOrder(buy);
+    REQUIRE(trades.size() == 0);
+    REQUIRE(book.size() == 0);
+}
